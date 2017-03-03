2230a9c486fe1750734e1c0dffe3a5a90563e1d5
Giuseppe Roberti <gr@silenti.net>
2017-03-03 17:37:29 +0100

    Update gumbo-query.rb
    Update gumbo-query brew formula.
    Use brew install --HEAD to install.

--- gumbo-query.rb
+++ gumbo-query.rb
@@ -1,15 +1,16 @@
 class GumboQuery < Formula
-  homepage "https://github.com/Falven/gumbo-query"
-  url "https://github.com/Falven/gumbo-query.git", :using => :git
-  sha256 "e772da6f700b29367d4ab7a7d462d89c0a6fff0e238c725eda8d9d7c44025839"
+  homepage "https://github.com/lazytiger/gumbo-query"
+  head "https://github.com/lazytiger/gumbo-query.git"
 
   depends_on "cmake" => :build
 
   def install
-    system "cd build"
-    system "cmake .."
-    system "make"
-    system "make install -DCMAKE_INSTALL_PREFIX:/usr/local/Cellar/gumbo-query/"
+    mkdir "build" do
+      args = std_cmake_args
+      system "cmake", "..", *args
+      system "make"
+      system "make", "install"
+    end
   end
 
   def test
